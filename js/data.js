3f587e1f94222e6f308efc425b66eb30954854f8
StefanoMDeveloper <stenmarti02@gmail.com>
2022-01-12 17:03:01 +0100

    finito

---
+++ js/data.js
@@ -0,0 +1,153 @@
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+
+let container = document.querySelector(".row");
+icons.forEach(element => {
+	container.innerHTML += `
+	<div class="col text-center p-2">
+		<i class="${element.family} ${element.prefix}${element.name}" style="color: ${element.color}"></i>
+		<p>${element.name}</p>
+    </div>
+	`
+});
+
+let filter = document.getElementById("filtro");
+
+filter.addEventListener("change", function(){
+	let select = document.getElementById("filtro").value;
+	container.innerHTML = "";
+
+	icons.forEach(element => {
+		if (select == element.color) {
+			container.innerHTML += `
+			<div class="col text-center p-2">
+				<i class="${element.family} ${element.prefix}${element.name}" style="color: ${element.color}"></i>
+				<p>${element.name}</p>
+			</div>
+			`
+		}
+
+		if (select == "all") {
+			container.innerHTML += `
+			<div class="col text-center p-2">
+				<i class="${element.family} ${element.prefix}${element.name}" style="color: ${element.color}"></i>
+				<p>${element.name}</p>
+			</div>
+			`
+		}
+	});
+	
+});
